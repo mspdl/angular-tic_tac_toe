736254f3a3e36d459ba34a757767885ae5544422
Morgan <morgan.espindola@gmail.com>
2023-09-28 21:33:22 -0300

    Created Tic Tac Toe Service

--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TicTacToeService } from './shared/tic-tac-toe.service';
 import { TicTacToeComponent } from './tic-tac-toe.component';
 
 @NgModule({
   declarations: [TicTacToeComponent],
   imports: [CommonModule],
   exports: [TicTacToeComponent],
+  providers: [TicTacToeService],
 })
 export class TicTacToeModule {}
