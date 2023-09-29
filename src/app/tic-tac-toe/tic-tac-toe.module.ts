558edc5b93f3857647e817e292f959a7898340ba
Morgan <morgan.espindola@gmail.com>
2023-09-28 21:31:03 -0300

    Created Tic Tac Toe Component

--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TicTacToeComponent } from './tic-tac-toe.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TicTacToeComponent],
   imports: [CommonModule],
+  exports: [TicTacToeComponent],
 })
 export class TicTacToeModule {}
